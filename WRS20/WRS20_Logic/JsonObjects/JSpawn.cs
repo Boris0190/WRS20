42d8533403d4f2b9a87e54554fd25f17475bf40c
boris <boris@4d455890-638a-fb47-986d-db43e2cb2079>
2013-01-12 10:46:42 +0000

    git-svn-id: http://borishome.thruhere.net:88/svn/wrs@3 4d455890-638a-fb47-986d-db43e2cb2079

---
+++ WRS20/WRS20_Logic/JsonObjects/JSpawn.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WRS20_Logic.JsonObjects
+{
+    class JSpawn : JsonObject
+    {
+        public JSpawn(String contentString)
+            : base(contentString)
+        { }
+        protected override void parseObjects()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
